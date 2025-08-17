03e43cfacf742c092ec8bbd3f9e2b8e72ccc348d
Shine Selorm <selomshine1@gmail.com>
2025-08-17 18:14:54 +0000

    Fix: prevent duplicate entry error in task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -1,8 +1,11 @@
--- TASK 5: INSERT A SINGLE ROW INTO CUSTOMERS TABLE
+-- TASK 5: INSERT A SINGLE ROW INTO CUSTOMER TABLE
 
 USE alx_book_store;
 
-INSERT INTO Customers (customer_id, customer_name, email, address)
+INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+
+
+
 
 
